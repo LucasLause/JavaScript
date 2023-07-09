c70e18ffc4e5ef28be7c75c157d57ace5e376e14
LucasLause <lucaslausedegregori@gmail.com>
2023-07-09 18:25:26 -0300

    segunda pre-entrega

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 alert("Bienvenid@ a mi tienda! :)")
 
+// Array de productos
 const productos = [
     {nombre: "Remera Linkin Park", precio: 5000 },
     {nombre: "Buzo Linkin Park", precio: 8000 },
@@ -7,41 +8,53 @@ const productos = [
     {nombre: "Vinilo Linkin Park", precio: 4000 }
 ]
 
+// Variables 
 let total = 0
 let continuar
 
+// Funcion para sumar el total de compra
 function sumaPrecio(totalCompra, precioProducto) {
     console.log(totalCompra, precioProducto)
     return totalCompra + precioProducto
 }
 
-
+// Bucle de compra
 do{
-    const opcion = parseInt(prompt(`Que producto desea comprar?:
-    1. Remera Linkin Park
-    2. Buzo Linkin Park
-    3. Disco Linkin Park
-    4. Vinilo Linkin Park`))
+    // Prompt eleccion de producto
+    let opcion = prompt(`Que producto desea comprar?:
+    - Remera Linkin Park
+    - Buzo Linkin Park
+    - Disco Linkin Park
+    - Vinilo Linkin Park`)
 
-    if(opcion === 1 ){
-        total = sumaPrecio(total, productos[0].precio)
-        continuar = prompt("Desea agregar otro producto? (si/no)").toLowerCase()
-    } else if(opcion === 2){
-        total = sumaPrecio(total, productos[1].precio)
-        continuar = prompt("Desea agregar otro producto? (si/no)").toLowerCase()
-    } else if(opcion === 3){
-        total = sumaPrecio(total, productos[2].precio)
-        continuar = prompt("Desea agregar otro producto? (si/no)").toLowerCase()
-    } else if(opcion === 4){
-        total = sumaPrecio(total, productos[3].precio)
-        continuar = prompt("Desea agregar otro producto? (si/no)").toLowerCase()
-    } else {
-        alert("Porfavor ingrese una opcion valida (ejemplo 1)")
-    } 
+    console.log(opcion)
 
+    // Find producto en Array
+    let productoCliente = productos.find(p => p.nombre === opcion)
 
+    // Si no encuentra el producto
+    if(!productoCliente){
+        alert("Producto no encontrado, porfavor revisar bien el nombre del producto deseado!")
+        opcion = prompt(`Que producto desea comprar?:
+        - Remera Linkin Park
+        - Buzo Linkin Park
+        - Disco Linkin Park
+        - Vinilo Linkin Park`)
+    
+        productoCliente = productos.find(p => p.nombre === opcion)
+    }
+
+    // Si encuentra el producto
+    console.log(productoCliente)
+    // Al total le mando funcion con total y precio del producto elegido
+    total = sumaPrecio(total, productoCliente.precio)
+    // Agregar otro producto o no
+    continuar = prompt("Desea agregar otro producto? (si/no)").toLowerCase()
+
+// Final del bucle
 } while(continuar !== "no" )
 
+// Alerta del total de la compra
 alert(`Gracias por tu compra! Tu total es de $${total}`)
 
 
