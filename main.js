54c205d890b026970a496e84cf61811c1bc27051
LucasLause <lucaslausedegregori@gmail.com>
2023-07-25 18:23:15 -0300

    Subida tercer preentrega

--- main.js
+++ main.js
@@ -1,16 +1,96 @@
-alert("Bienvenid@ a mi tienda! :)")
 
 // Array de productos
 const productos = [
-    {nombre: "Remera Linkin Park", precio: 5000 },
-    {nombre: "Buzo Linkin Park", precio: 8000 },
-    {nombre: "Disco Linkin Park", precio: 2000 },
-    {nombre: "Vinilo Linkin Park", precio: 4000 }
+    {id: 1, nombre: "Remera Linkin Park", precio: 5000 },
+    {id: 2, nombre: "Buzo Linkin Park", precio: 8000 },
+    {id: 3, nombre: "Disco Linkin Park", precio: 2000 },
+    {id: 4, nombre: "Vinilo Linkin Park", precio: 4000 }
 ]
+
+// Guardar producto en local
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+// Guardar array completo
+guardarLocal("listaProductos", JSON.stringify(productos));
+
+// Recuperar array de objetos
+const prods = localStorage.getItem("listaProductos")
+const parseProds = JSON.parse(prods)
+
+// Agarro id del index
+const divMerch = document.getElementById('div-merch')
+
+// Inserto HTML a ese id del index con el array recuperado y parseado
+parseProds.forEach(p => {
+    divMerch.innerHTML += `
+    <div style="border: 1px solid black; width: 16%; margin-bottom:20px; padding-left: 20px; padding-bottom:20px">
+        <h3>${p.nombre}</h3>
+        <p>$${p.precio}</p>
+        <button id="boton${p.id}">Agregar al Carrito</button>
+    </div>
+    `
+});
+
+// Agarro los botones
+const boton1 = document.getElementById("boton1")
+const boton2 = document.getElementById("boton2")
+const boton3 = document.getElementById("boton3")
+const boton4 = document.getElementById("boton4")
+
+// Le agrego event listeners a los botones
+boton1.addEventListener("click", click1)
+boton2.addEventListener("click", click2)
+boton3.addEventListener("click", click3)
+boton4.addEventListener("click", click4)
+
+// Agarro div de carrito
+const divCarrito = document.getElementById('div-carrito')
+const totalCompra = document.getElementById('total')
+
 
 // Variables 
 let total = 0
-let continuar
+
+// Respuesta botones
+function click1(){
+    total = total + productos[0].precio
+    totalCompra.textContent = 'Total:' + ' ' + total
+    divCarrito.innerHTML += `
+    <ul>
+        <li>${productos[0].nombre} $${productos[0].precio}</li>
+    </ul>
+    `
+}
+
+function click2(){
+    total = total + productos[1].precio
+    totalCompra.textContent = 'Total:' + ' '  + total
+    divCarrito.innerHTML += `
+    <ul>
+        <li>${productos[1].nombre} $${productos[1].precio}</li>
+    </ul>
+    `
+}
+
+function click3(){
+    total = total + productos[2].precio
+    totalCompra.textContent = 'Total:' + ' '  + total
+    divCarrito.innerHTML += `
+    <ul>
+        <li>${productos[2].nombre} $${productos[2].precio}</li>
+    </ul>
+    `
+}
+
+function click4(){
+    total = total + productos[3].precio
+    totalCompra.textContent = 'Total:' + ' '  + total
+    divCarrito.innerHTML += `
+    <ul>
+        <li>${productos[3].nombre} $${productos[3].precio}</li>
+    </ul>
+    `
+}
+
 
 // Funcion para sumar el total de compra
 function sumaPrecio(totalCompra, precioProducto) {
@@ -18,44 +98,8 @@ function sumaPrecio(totalCompra, precioProducto) {
     return totalCompra + precioProducto
 }
 
-// Bucle de compra
-do{
-    // Prompt eleccion de producto
-    let opcion = prompt(`Que producto desea comprar?:
-    - Remera Linkin Park
-    - Buzo Linkin Park
-    - Disco Linkin Park
-    - Vinilo Linkin Park`)
 
-    console.log(opcion)
 
-    // Find producto en Array
-    let productoCliente = productos.find(p => p.nombre === opcion)
-
-    // Si no encuentra el producto
-    if(!productoCliente){
-        alert("Producto no encontrado, porfavor revisar bien el nombre del producto deseado!")
-        opcion = prompt(`Que producto desea comprar?:
-        - Remera Linkin Park
-        - Buzo Linkin Park
-        - Disco Linkin Park
-        - Vinilo Linkin Park`)
-    
-        productoCliente = productos.find(p => p.nombre === opcion)
-    }
-
-    // Si encuentra el producto
-    console.log(productoCliente)
-    // Al total le mando funcion con total y precio del producto elegido
-    total = sumaPrecio(total, productoCliente.precio)
-    // Agregar otro producto o no
-    continuar = prompt("Desea agregar otro producto? (si/no)").toLowerCase()
-
-// Final del bucle
-} while(continuar !== "no" )
-
-// Alerta del total de la compra
-alert(`Gracias por tu compra! Tu total es de $${total}`)
 
 
 
